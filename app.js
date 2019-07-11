5c901a58c326adda56921fad49355e8b3200bdaa
AJ Speller <ajspeller@gmail.com>
2019-07-11 11:54:19 -0400

    Added headers to handle browser security related to CORS

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use('/products', productsRouter);
 app.use('/orders', ordersRouter);
 
