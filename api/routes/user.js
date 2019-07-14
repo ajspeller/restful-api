43cefc05b1d778260c7a1245582c87496a44115f
AJ Speller <ajspeller@gmail.com>
2019-07-14 15:57:40 -0400

    Added checkAuth authentication middleware to user endpoint

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
+const checkAuth = require('../auth/check-auth');
+
 const Usercontroller = require('../controllers/user.controller');
 
 const router = express.Router();
 
 router.post('/signup', Usercontroller.signup);
 router.post('/login', Usercontroller.login);
-router.delete('/:id', Usercontroller.user_delete);
+router.delete('/:id', checkAuth, Usercontroller.user_delete);
 router.get('/', Usercontroller.user_retrieve);
 
 module.exports = router;
